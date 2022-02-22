462264629cd2593d93343a63335baf1ff551a534
Shrikant Badiger <shrikant200@gmail.com>
2022-02-22 18:01:59 +0530

    Update invoice.component.ts

--- frontend/src/app/invoice/invoice.component.ts
+++ frontend/src/app/invoice/invoice.component.ts
@@ -1,3 +1,4 @@
+import { ValueConverter } from '@angular/compiler/src/render3/view/template';
 import { Component, OnInit } from '@angular/core';
 import { Router, ActivatedRoute, Data } from '@angular/router';
 import {NgxPrintModule} from 'ngx-print';
@@ -15,42 +16,91 @@ export class InvoiceComponent implements OnInit {
   wieght:number;
   makingCharge:number;
   totalAmount:number;
-  totalBalance:number;
+  totalPayable: number;
   finalAmount:string;
   totalAdvance:number;
+  calucatedAmount: number;
+  calculatedCgst: number;
+  calculatedSgst: number;
   todayDate : Date = new Date();
   invoiceNum:number= Date.now();
   constructor(private router: Router) { 
     this.totalAmount=0;
-    this.totalBalance=0;
+    this.totalPayable = 0;
     this.totalAdvance=0;
   }
   
   private orderList: Array<any> = [];
   private newAttribute: any = {};
+  updateTotal_byCGST(cgst: string) {
+    if (isNaN(parseFloat(cgst))) {
+      this.totalPayable = this.totalPayable;
+      this.get_calculateCgst("0")
+    } else {
+      this.totalPayable = this.totalAmount + (this.totalAmount * parseFloat(cgst) / 100);
+      this.get_calculateCgst(cgst);
+    }
+  }
+  get_calculateCgst(cgstPercentage: string) {
+    this.calculatedCgst = this.totalAmount * parseFloat(cgstPercentage) / 100;
+  }
+  get_calculateSgst(sgstPercentage: string) {
+    this.calculatedSgst = this.totalAmount * parseFloat(sgstPercentage) / 100;
+  }
+  updateTotal_bySGST(sgst: string) {
+    if (isNaN(parseFloat(sgst))) {
+      this.totalPayable = this.totalPayable;
+      this.get_calculateSgst("0");
+    } else {
+      this.totalPayable = this.totalAmount + (this.totalAmount * parseFloat(sgst) / 100);
+      this.get_calculateSgst(sgst);
+    }
+  }
 
-  
-    addFieldValue(Id:string,Descp:string,Quant:string,Wt:string, MkCharge:string,Amt:string)
+  getAmt(Wt: string, Pr: string, MkCharge: string) {
+    if (isNaN(parseFloat(Pr)) || isNaN(parseFloat(MkCharge)) || isNaN(parseFloat(Wt))) {
+      return 0
+    }
+    else {
+      return (parseFloat(Wt) * parseFloat(Pr) + parseFloat(MkCharge));
+    }
+  }
+  addFieldValue(Id: string, Descp: string, Wt: string, Pr: string, MkCharge: string)
     {
-        this.newAttribute={"id":Id,"description":Descp,"quantity":Quant,"weight":Wt,"makingCharge":MkCharge,"amount":Amt}
+    this.newAttribute = { "id": Id, "description": Descp, "weight": Wt, "price": Pr, "makingCharge": MkCharge, "amount": this.getAmt(Wt, Pr, MkCharge) }
         console.log(this.newAttribute);
         this.orderList.push(this.newAttribute)
         this.newAttribute = {};
-        this.totalAmount=this.totalAmount+parseInt(Amt);
-        this.totalBalance=this.totalAmount;
+    this.totalAmount = this.totalAmount + (this.getAmt(Wt, Pr, MkCharge));
+    this.totalPayable = this.totalAmount;
+    this.calucatedAmount = 0;
     }
    
-
+  getAmount(iWeight: string, iPrice: string, iMakingCharge: string) {
+    console.log(" I am inside the getAmount")
+    this.calucatedAmount = 0
+    console.log(parseFloat(iWeight));
+    console.log(parseFloat(iPrice));
+    console.log(parseFloat(iMakingCharge))
+    if (isNaN(parseFloat(iPrice)) || isNaN(parseFloat(iMakingCharge))) {
+      this.calucatedAmount = parseFloat(iWeight);
+    }
+    else {
+      this.calucatedAmount = parseFloat(iWeight) * parseFloat(iPrice) + parseFloat(iMakingCharge)
+    }
+  }
     deleteFieldValue(index) {
         console.log("Deleting the value");
-        this.totalAmount=this.totalAmount-parseInt(this.orderList[index].amount);
+      this.totalAmount = this.totalAmount - parseFloat(this.orderList[index].amount);
         this.orderList.splice(index, 1);
     }
     addAdvance(advance='0'){
       console.log("Advance Amount");
-      this.totalAdvance=parseInt(advance);
-      this.totalBalance=this.totalAmount-this.totalAdvance;
+      this.totalAdvance = parseFloat(advance);
+      this.totalPayable = this.totalAmount - this.totalAdvance;
     }
+
+
   ngOnInit() {
   
   }
